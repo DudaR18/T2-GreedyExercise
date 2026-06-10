b9841c3a1dab1d4fd3d8dd8e85bf22292ce84c66
Eduarda Rubin <141246640+DudaR18@users.noreply.github.com>
2026-06-10 00:33:34 +0000

    algoritmo pronto

--- RallyGuloso.java
+++ RallyGuloso.java
@@ -1,16 +1,15 @@
-import java.util*;
+import java.util.*;
 
 public class RallyGuloso {
 
-    //trilha do rally de comprimento 𝐿
-    //em um conjunto de pokntos a distância entre cada par adjacente é no máximo d
-    public static List<Integer> rallyguloso (int L, int d, int[] pontos){
+    // trilha do rally de comprimento 𝐿
+    // em um conjunto de pontos a distância entre cada par adjacente é no máximo d
+    public static List<Integer> rallyguloso(int L, int d, int[] pontos) {
 
-        int atual = 0; 
-        int indice = 0; 
+        int atual = 0;
+        int indice = 0;
 
         List<Integer> paradas = new ArrayList<>(); // pontos de paradas 𝑥1, 𝑥2, … , 𝑥n do ponto de partida
-
 
         while (atual + d < L) {
 
@@ -24,11 +23,31 @@ public class RallyGuloso {
             }
 
             if (ultimo == atual) {
-                return null; //n existe solucao
+                return null; // n existe solucao
             }
 
             paradas.add(ultimo);
             atual = ultimo;
+        }
+
+        return paradas;
+    }
+
+    public static void main(String[] args) {
+
+        int L = 25; // comprimento da rally
+        int d = 10; // distancia q podemos andar antes de anoitecer
+
+        int[] pontos = { 5, 9, 13, 18, 24 };
+
+        List<Integer> resultado = rallyguloso(L, d, pontos);
+
+        if (resultado == null) {
+            System.out.println("Não existe solução.");
+        } else {
+
+            System.out.println("Paradas: " + resultado);
+            System.out.println("Quantidade de paradas: " + resultado.size());
         }
     }
 }
